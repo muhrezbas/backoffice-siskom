80789fd195eb01885303c77b553bdd6a4f5469d8
bagusbimo <bagus.b@mail.ugm.ac.id>
2021-08-02 16:27:15 +0700

    add profile view

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -2,156 +2,145 @@
   <title-bar :title-stack="titleStack" />
   <hero-bar>Profile</hero-bar>
   <main-section>
+    <div class="grid grid-cols-1 gap-6 lg:grid-cols-3 mb-6">
+      <card-widget-two
+        class="tile"
+        color="text-green-500"
+        :icon="mdiCashMultiple"
+        :numberOne="280000"
+        :numberTwo="280"
+        :prefixOne="'Rp '"
+        :prefixTwo="'SMS : '"
+        label="Available balance"
+      />
+    </div>
 
-    <card-component
-      title="Edit Profile"
-      class="lg:w-8/12 lg:mx-auto"
-      :icon="mdiAccountCircle"
-      @submit.prevent="submitProfile"
-      form
-    >
-      <field label="Avatar" help="Max 500kb">
-        <file-picker />
-      </field>
-
-      <field label="Name" help="Required. Your name">
-        <control :icon="mdiAccount" v-model="profileForm.name" name="username" required autocomplete="username"/>
-      </field>
-      <field label="E-mail" help="Required. Your e-mail">
-        <control :icon="mdiMail" type="email" name="email" v-model="profileForm.email" required autocomplete="email"/>
-      </field>
-
-      <divider/>
-
-      <jb-buttons>
-        <jb-button color="info" type="submit" label="Submit" />
-        <jb-button color="info" label="Options" outline />
-      </jb-buttons>
-    </card-component>
-
-    <card-component
-      title="Change Password"
-      class="lg:w-8/12 lg:mx-auto"
-      :icon="mdiLock"
-      @submit.prevent="submitPass"
-      form
-    >
-      <field label="Current password" help="Required. Your current password">
-        <control
-          :icon="mdiAsterisk"
-          v-model="passwordForm.password_current"
-          name="password_current"
-          type="password"
-          required
-          autocomplete="current-password"
+    <div class="grid grid-cols-1 gap-6 lg:grid-cols-2 mb-6">
+      <card-component class="h-full flex items-center">
+        <img
+          src="https://www.freepnglogos.com/uploads/zara-brand/clothing-brand-zara-hd-zara-logo-2020-3.jpg"
         />
-      </field>
-
-      <divider/>
-
-      <field label="New password" help="Required. New password">
-        <control
-          :icon="mdiFormTextboxPassword"
-          v-model="passwordForm.password"
-          name="password"
-          type="password"
-          required
-          autocomplete="new-password"
-        />
-      </field>
-
-      <field label="Confirm password" help="Required. New password one more time">
-        <control
-          :icon="mdiFormTextboxPassword"
-          v-model="passwordForm.password_confirmation"
-          name="password_confirmation"
-          type="password"
-          required
-          autocomplete="new-password"
-        />
-      </field>
-
-      <divider/>
-
-      <jb-buttons>
-        <jb-button type="submit" color="info" label="Submit" />
-        <jb-button color="info" label="Options" outline />
-      </jb-buttons>
-
-    </card-component>
+        <div class="mt-4 text-xl font-bold">
+          Zara
+        </div>
+        <div class="mt-1 text-base">
+          Fashion Cloth
+        </div>
+        <div class="mt-6 text-gray-500">
+          Zara adalah salah satu merek yg berasal dari Spanyol dan bermarkas di
+          Arteixo, Gallicia. Zara didirikan pada tahun 1975 oleh Armancio Ortega
+          dan Rosallia mera...
+        </div>
+      </card-component>
+      <div>
+        <card-component
+          title="Performance"
+          :icon="mdiFinance"
+          :header-icon="mdiReload"
+          class="mb-6"
+          @header-icon-click="fillChartData"
+        >
+          <div v-if="chartData">
+            <line-chart :data="chartData" class="h-96" />
+          </div>
+        </card-component>
+        <div class="grid grid-cols-1 gap-6 lg:grid-cols-2">
+          <card-widget
+            class="tile"
+            color="text-blue-500"
+            :icon="mdiCellphoneMessage"
+            :number="1300"
+            label="SMS Blast Delivered"
+          />
+          <card-widget
+            class="tile"
+            color="text-blue-500"
+            :icon="mdiCellphoneText"
+            :number="2300"
+            label="OTP Delivered"
+          />
+        </div>
+      </div>
+    </div>
   </main-section>
 
-  <bottom-other-pages-section />
+  <hero-bar :control="true">History</hero-bar>
+
+  <main-section>
+    <card-component has-table>
+      <sms-table checkable />
+    </card-component>
+  </main-section>
 </template>
 
 <script>
-import { ref, reactive } from 'vue'
-import { useStore } from 'vuex'
-import { mdiAccount, mdiAccountCircle, mdiLock, mdiMail, mdiAsterisk, mdiFormTextboxPassword } from '@mdi/js'
-import MainSection from '@/components/MainSection'
-import CardComponent from '@/components/CardComponent'
-import TitleBar from '@/components/TitleBar'
-import HeroBar from '@/components/HeroBar'
-import Divider from '@/components/Divider'
-import Field from '@/components/Field'
-import Control from '@/components/Control'
-import FilePicker from '@/components/FilePicker'
-import JbButton from '@/components/JbButton'
-import BottomOtherPagesSection from '@/components/BottomOtherPagesSection'
-import JbButtons from '@/components/JbButtons'
+/* eslint-disable */
+// @ is an alias to /src
+import { ref, onMounted } from "vue";
+import {
+  mdiAccountMultiple,
+  mdiCashMultiple,
+  mdiCellphoneText,
+  mdiCellphoneMessage,
+  mdiChartTimelineVariant,
+  mdiFinance,
+  mdiMonitorCellphone,
+  mdiReload,
+  mdiGithub
+} from "@mdi/js";
+import * as chartConfig from "@/components/Charts/chart.config";
+import LineChart from "@/components/Charts/LineChart";
+import MainSection from "@/components/MainSection";
+import TitleBar from "@/components/TitleBar";
+import HeroBar from "@/components/HeroBar";
+import CardWidget from "@/components/CardWidget";
+import CardWidgetTwo from "@/components/CardWidgetTwo";
+import CardComponent from "@/components/CardComponent";
+import SmsTable from "@/components/SmsTable";
+import Notification from "@/components/Notification";
+import JbButton from "@/components/JbButton";
 
 export default {
-  name: 'Profile',
+  name: "Profile",
   components: {
-    JbButtons,
     MainSection,
+    SmsTable,
+    LineChart,
+    CardComponent,
+    CardWidget,
+    CardWidgetTwo,
     HeroBar,
     TitleBar,
-    CardComponent,
-    Divider,
-    Field,
-    Control,
-    FilePicker,
-    JbButton,
-    BottomOtherPagesSection
+    Notification,
+    JbButton
   },
-  setup () {
-    const store = useStore()
+  setup() {
+    const titleStack = ref(["Admin", "Profile"]);
 
-    const titleStack = ref(['Admin', 'Profile'])
+    const chartData = ref(null);
 
-    const profileForm = reactive({
-      name: store.state.userName,
-      email: store.state.userEmail
-    })
+    const fillChartData = () => {
+      chartData.value = chartConfig.sampleChartData();
+    };
 
-    const passwordForm = reactive({
-      password_current: '',
-      password: '',
-      password_confirmation: ''
-    })
-
-    const submitProfile = () => {
-      store.commit('user', profileForm)
-    }
-
-    const submitPass = () => {
-      //
-    }
+    onMounted(() => {
+      fillChartData();
+    });
 
     return {
       titleStack,
-      profileForm,
-      passwordForm,
-      submitProfile,
-      submitPass,
-      mdiAccount,
-      mdiAccountCircle,
-      mdiLock,
-      mdiMail,
-      mdiAsterisk,
-      mdiFormTextboxPassword
-    }
+      chartData,
+      fillChartData,
+      mdiAccountMultiple,
+      mdiCashMultiple,
+      mdiCellphoneText,
+      mdiCellphoneMessage,
+      mdiChartTimelineVariant,
+      mdiFinance,
+      mdiMonitorCellphone,
+      mdiReload,
+      mdiGithub
+    };
   }
-}
+};
 </script>
