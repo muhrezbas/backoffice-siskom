36fde7b5bf891072d88bd56a969155e9a6b1dedf
bagusbimo <bagus.b@mail.ugm.ac.id>
2021-08-03 12:46:54 +0700

    add invoice route

--- src/router/index.js
+++ src/router/index.js
@@ -1,85 +1,97 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
-import Home from '../views/Home'
+/* eslint-disable */
+import { createRouter, createWebHashHistory } from "vue-router";
+import Home from "../views/Home";
 
 const routes = [
   {
     // Document title tag
     // We combine it with defaultDocumentTitle set in `src/main.js` on router.afterEach hook
     meta: {
-      title: 'Dashboard'
+      title: "Dashboard"
     },
-    path: '/',
-    name: 'home',
+    path: "/",
+    name: "home",
     component: Home
   },
   {
     meta: {
-      title: 'Tables'
+      title: "Tables"
     },
-    path: '/tables',
-    name: 'tables',
+    path: "/tables",
+    name: "tables",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "tables" */ '../views/Tables')
+    component: () => import(/* webpackChunkName: "tables" */ "../views/Tables")
   },
   {
     meta: {
-      title: 'SMS Detail'
+      title: "SMS Detail"
     },
-    path: '/smsdetail',
-    name: 'smsdetail',
-    component: () => import(/* webpackChunkName: "forms" */ '../views/SmsDetail')
+    path: "/smsdetail",
+    name: "smsdetail",
+    component: () =>
+      import(/* webpackChunkName: "forms" */ "../views/SmsDetail")
   },
   {
     meta: {
-      title: 'SMS Input'
+      title: "SMS Input"
     },
-    path: '/smsinput',
-    name: 'smsinput',
-    component: () => import(/* webpackChunkName: "forms" */ '../views/SmsInput')
+    path: "/smsinput",
+    name: "smsinput",
+    component: () => import(/* webpackChunkName: "forms" */ "../views/SmsInput")
   },
   {
     meta: {
-      title: 'Forms'
+      title: "Forms"
     },
-    path: '/forms',
-    name: 'forms',
-    component: () => import(/* webpackChunkName: "forms" */ '../views/Forms')
+    path: "/forms",
+    name: "forms",
+    component: () => import(/* webpackChunkName: "forms" */ "../views/Forms")
   },
   {
     meta: {
-      title: 'Profile'
+      title: "Profile"
     },
-    path: '/profile',
-    name: 'profile',
-    component: () => import(/* webpackChunkName: "profile" */ '../views/Profile')
+    path: "/profile",
+    name: "profile",
+    component: () =>
+      import(/* webpackChunkName: "profile" */ "../views/Profile")
   },
   {
     meta: {
-      title: 'Ui'
+      title: "Invoice"
     },
-    path: '/ui',
-    name: 'ui',
-    component: () => import(/* webpackChunkName: "profile" */ '../views/Ui')
+    path: "/invoice",
+    name: "invoice",
+    component: () =>
+      import(/* webpackChunkName: "profile" */ "../views/Invoice")
   },
   {
     meta: {
-      title: 'Login',
+      title: "Ui"
+    },
+    path: "/ui",
+    name: "ui",
+    component: () => import(/* webpackChunkName: "profile" */ "../views/Ui")
+  },
+  {
+    meta: {
+      title: "Login",
       formScreen: true
     },
-    path: '/login',
-    name: 'login',
-    component: () => import(/* webpackChunkName: "login" */ '../views/Login')
+    path: "/login",
+    name: "login",
+    component: () => import(/* webpackChunkName: "login" */ "../views/Login")
   }
-]
+];
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes,
-  scrollBehavior (to, from, savedPosition) {
-    return savedPosition || { top: 0 }
+  scrollBehavior(to, from, savedPosition) {
+    return savedPosition || { top: 0 };
   }
-})
+});
 
-export default router
+export default router;
