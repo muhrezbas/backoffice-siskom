edc54b20442c101a5a3ac598e1e2a6122c6d6e9d
Bagus Bimo <bagus.mulyanto@dompetkilat.co.id>
2021-08-23 16:08:44 +0700

    remove eye icon smstable

--- src/components/SmsTable.vue
+++ src/components/SmsTable.vue
@@ -6,43 +6,43 @@
 
   <table>
     <thead>
-    <tr>
-      <th></th>
-      <th>Supplier ID</th>
-      <th>SMS ID</th>
-      <th>SMS Time</th>
-      <th>SMS Date</th>
-      <th>Phone No.</th>
-      <th>Words</th>
-      <th>Status</th>
-      <th>Type</th>
-      <th></th>
-    </tr>
+      <tr>
+        <th></th>
+        <th>Supplier ID</th>
+        <th>SMS ID</th>
+        <th>SMS Time</th>
+        <th>SMS Date</th>
+        <th>Phone No.</th>
+        <th>Words</th>
+        <th>Status</th>
+        <th>Type</th>
+        <!-- <th></th> -->
+      </tr>
     </thead>
     <tbody class="font-semibold">
-    <tr v-for="sms in itemsPaginated" :key="sms.id">
-      <td>{{ sms.id }}</td>
-      <td data-label="SupplierID">{{ sms.supplierId }}</td>
-      <td data-label="SmsId">{{ sms.smsId }}</td>
-      <td data-label="SmsTime">{{ sms.smsTime }}</td>
-      <td data-label="SmsDate">{{ sms.smsDate }}</td>
-      <td data-label="PhoneNo">{{ sms.phoneNo }}</td>
-      <td data-label="Words">{{ sms.words }}</td>
-      <td data-label="Status">
+      <tr v-for="sms in itemsPaginated" :key="sms.id">
+        <td>{{ sms.id }}</td>
+        <td data-label="SupplierID">{{ sms.supplierId }}</td>
+        <td data-label="SmsId">{{ sms.smsId }}</td>
+        <td data-label="SmsTime">{{ sms.smsTime }}</td>
+        <td data-label="SmsDate">{{ sms.smsDate }}</td>
+        <td data-label="PhoneNo">{{ sms.phoneNo }}</td>
+        <td data-label="Words">{{ sms.words }}</td>
+        <td data-label="Status">
           <p v-if="sms.status == 0" class="text-red-700">Error</p>
           <p v-if="sms.status == 1" class="text-blue-300">Success</p>
           <p v-if="sms.status == 2" class="text-purple-700">Pending</p>
-      </td>
-      <td data-label="Type">
+        </td>
+        <td data-label="Type">
           <p v-if="sms.type == 1" class="text-yellow-500">OTP</p>
           <p v-if="sms.type == 2" class="text-blue-500">SMS Blast</p>
-      </td>
-      <td class="actions-cell">
+        </td>
+        <!-- <td class="actions-cell">
         <jb-buttons type="justify-start lg:justify-end" no-wrap>
           <jb-button class="mr-3" color="info" :icon="mdiEye" small @click="isModalActive = true" />
         </jb-buttons>
-      </td>
-    </tr>
+      </td> -->
+      </tr>
     </tbody>
   </table>
   <div class="table-pagination">
@@ -63,52 +63,58 @@
 </template>
 
 <script>
-import { computed, ref } from 'vue'
-import { useStore } from 'vuex'
-import { mdiEye, mdiTrashCan } from '@mdi/js'
-import ModalBox from '@/components/ModalBox'
-import Level from '@/components/Level'
-import JbButtons from '@/components/JbButtons'
-import JbButton from '@/components/JbButton'
+/* eslint-disable */
+import { computed, ref } from "vue";
+import { useStore } from "vuex";
+import { mdiEye, mdiTrashCan } from "@mdi/js";
+import ModalBox from "@/components/ModalBox";
+import Level from "@/components/Level";
+import JbButtons from "@/components/JbButtons";
+import JbButton from "@/components/JbButton";
 
 export default {
-  name: 'SmsTable',
+  name: "SmsTable",
   components: {
     ModalBox,
     Level,
     JbButtons,
     JbButton
   },
-  setup () {
-    const store = useStore()
+  setup() {
+    const store = useStore();
 
-    const items = computed(() => store.state.sms)
+    const items = computed(() => store.state.sms);
 
-    const isModalActive = ref(false)
+    const isModalActive = ref(false);
 
-    const perPage = ref(10)
+    const perPage = ref(10);
 
-    const currentPage = ref(0)
+    const currentPage = ref(0);
 
-    const checkedRows = ref([])
+    const checkedRows = ref([]);
 
-    const itemsPaginated = computed(
-      () => items.value.slice(perPage.value * currentPage.value, perPage.value * (currentPage.value + 1))
-    )
+    const itemsPaginated = computed(() =>
+      items.value.slice(
+        perPage.value * currentPage.value,
+        perPage.value * (currentPage.value + 1)
+      )
+    );
 
-    const numPages = computed(() => Math.ceil(items.value.length / perPage.value))
+    const numPages = computed(() =>
+      Math.ceil(items.value.length / perPage.value)
+    );
 
-    const currentPageHuman = computed(() => currentPage.value + 1)
+    const currentPageHuman = computed(() => currentPage.value + 1);
 
     const pagesList = computed(() => {
-      const pagesList = []
+      const pagesList = [];
 
       for (let i = 0; i < numPages.value; i++) {
-        pagesList.push(i)
+        pagesList.push(i);
       }
 
-      return pagesList
-    })
+      return pagesList;
+    });
 
     return {
       isModalActive,
@@ -120,7 +126,7 @@ export default {
       pagesList,
       mdiEye,
       mdiTrashCan
-    }
+    };
   }
-}
+};
 </script>
