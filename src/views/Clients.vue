5b535bbd0f63bebcdefae05480d523a7e3275e83
Bagus Bimo <bagus.mulyanto@dompetkilat.co.id>
2021-08-29 14:03:44 +0700

    add overflow y for param modal on clients

--- src/views/Clients.vue
+++ src/views/Clients.vue
@@ -1,77 +1,141 @@
 <template>
   <modal-box v-model="paramWindow" title="Set Parameter" :submit="postClient">
-    <div class="flex flex-wrap -mx-3 mb-6">
-      <field label="Username" class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-        <control v-model="userData.username" name="prize" required autocomplete="prize" />
-      </field>
+    <div
+      class="overflow-y-auto overflow-x-hidden max-h-paramMobile md:max-h-paramDesktop"
+    >
+      <div class="flex flex-wrap -mx-3 mb-6">
+        <field label="Username" class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+          <control
+            v-model="userData.username"
+            name="prize"
+            required
+            autocomplete="prize"
+          />
+        </field>
 
-      <field label="Company Name" class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-        <control v-model="userData.companyName" name="prize" required autocomplete="prize" />
-      </field>
-    </div>
-
-    <div class="flex flex-wrap -mx-3 mb-6">
-      <field label="Company Phone Number" class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-        <control v-model="userData.companyNumber" name="prize" required autocomplete="prize" />
-      </field>
-
-      <field label="Company Address" class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-        <control v-model="userData.companyAddress" name="prize" required autocomplete="prize" />
-      </field>
-    </div>
-    <div class="flex flex-wrap -mx-3 mb-6">
-      <field label="Name PIC" class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-        <control v-model="userData.namePic" name="prize" required autocomplete="prize" />
-      </field>
-
-      <field label="Contact PIC" class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-        <control v-model="userData.contactPic" name="prize" required autocomplete="prize" />
-      </field>
-    </div>
-    <div class="flex flex-wrap -mx-3 mb-6">
-      <field label="Whitelist IP" class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-        <control v-model="userData.whiteListIp" name="prize" required autocomplete="prize" />
-      </field>
-
-      <field label="Finance Email" class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-        <control v-model="userData.financeEmail" name="prize" required autocomplete="prize" />
-      </field>
-    </div>
-    <div class="flex flex-wrap -mx-3 mb-6">
-      <field label="Notification Email" class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-        <control v-model="userData.notifEmail" name="prize" required autocomplete="prize" />
-      </field>
-
-      <field label="Password" class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-        <control v-model="userData.password" name="prize" required autocomplete="prize" />
-      </field>
-    </div>
-    <!-- <div class="flex flex-wrap -mx-3 mb-6"> -->
-    <field label="Priority Protocol">
-      <div>
-        <select
-          v-model="userData.priority[index]"
-          class="w-1/4 mb-2 ml-1"
-          v-for="(deta, index) in $store.state.protocol"
-          :key="index"
-        >
-          <option
-            v-for="option in $store.state.protocol"
-            :key="option._id ?? option"
-            :value="option._id"
-          >{{ option.supplier ?? option }}</option>
-        </select>
+        <field label="Company Name" class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+          <control
+            v-model="userData.companyName"
+            name="prize"
+            required
+            autocomplete="prize"
+          />
+        </field>
       </div>
-    </field>
 
-    <!-- <field label="Prize" class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+      <div class="flex flex-wrap -mx-3 mb-6">
+        <field
+          label="Company Phone Number"
+          class="w-full md:w-1/2 px-3 mb-6 md:mb-0"
+        >
+          <control
+            v-model="userData.companyNumber"
+            name="prize"
+            required
+            autocomplete="prize"
+          />
+        </field>
+
+        <field
+          label="Company Address"
+          class="w-full md:w-1/2 px-3 mb-6 md:mb-0"
+        >
+          <control
+            v-model="userData.companyAddress"
+            name="prize"
+            required
+            autocomplete="prize"
+          />
+        </field>
+      </div>
+      <div class="flex flex-wrap -mx-3 mb-6">
+        <field label="Name PIC" class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+          <control
+            v-model="userData.namePic"
+            name="prize"
+            required
+            autocomplete="prize"
+          />
+        </field>
+
+        <field label="Contact PIC" class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+          <control
+            v-model="userData.contactPic"
+            name="prize"
+            required
+            autocomplete="prize"
+          />
+        </field>
+      </div>
+      <div class="flex flex-wrap -mx-3 mb-6">
+        <field label="Whitelist IP" class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+          <control
+            v-model="userData.whiteListIp"
+            name="prize"
+            required
+            autocomplete="prize"
+          />
+        </field>
+
+        <field label="Finance Email" class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+          <control
+            v-model="userData.financeEmail"
+            name="prize"
+            required
+            autocomplete="prize"
+          />
+        </field>
+      </div>
+      <div class="flex flex-wrap -mx-3 mb-6">
+        <field
+          label="Notification Email"
+          class="w-full md:w-1/2 px-3 mb-6 md:mb-0"
+        >
+          <control
+            v-model="userData.notifEmail"
+            name="prize"
+            required
+            autocomplete="prize"
+          />
+        </field>
+
+        <field label="Password" class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+          <control
+            v-model="userData.password"
+            name="prize"
+            required
+            autocomplete="prize"
+          />
+        </field>
+      </div>
+      <!-- <div class="flex flex-wrap -mx-3 mb-6"> -->
+      <field label="Priority Protocol">
+        <div>
+          <select
+            v-model="userData.priority[index]"
+            class="w-1/4 mb-2 ml-1"
+            v-for="(deta, index) in $store.state.protocol"
+            :key="index"
+          >
+            <option
+              v-for="option in $store.state.protocol"
+              :key="option._id ?? option"
+              :value="option._id"
+              >{{ option.supplier ?? option }}</option
+            >
+          </select>
+        </div>
+      </field>
+
+      <!-- <field label="Prize" class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
         <control v-model="userData.total" name="prize" required autocomplete="prize" />
       </field>
 
       <field label="Prize" class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
         <control v-model="userData.total" name="prize" required autocomplete="prize" />
     </field>-->
-    <!-- </div> -->
+      <!-- </div> -->
+    </div>
   </modal-box>
   <title-bar :title-stack="titleStack" />
   <hero-bar class="mb-5">Settings</hero-bar>
@@ -98,8 +162,8 @@
 // @ is an alias to /src
 import { ref, onMounted, computed, reactive } from "vue";
 import { useStore } from "vuex";
-import Swal from 'sweetalert2'
-import axios from 'axios'
+import Swal from "sweetalert2";
+import axios from "axios";
 import {
   mdiAccountMultiple,
   mdiCashMultiple,
@@ -160,113 +224,84 @@ export default {
     const userData = computed(() =>
       reactive({
         priority: [],
-        username
-          :
-          "",
-        companyName
-          :
-          "",
-        companyNumber
-          :
-          "",
-        companyAddress
-          :
-          "",
-        namePic
-          :
-          "",
-        contactPic
-          :
-          "",
-        whiteListIp
-          :
-          "",
-        financeEmail
-          :
-          "",
-        notifEmail
-          :
-          "",
-        password
-          :
-          "",
-
+        username: "",
+        companyName: "",
+        companyNumber: "",
+        companyAddress: "",
+        namePic: "",
+        contactPic: "",
+        whiteListIp: "",
+        financeEmail: "",
+        notifEmail: "",
+        password: ""
       })
-    )
+    );
 
     const fillChartData = () => {
       chartData.value = chartConfig.sampleChartData();
     };
     const postClient = () => {
-      console.log(userData.value)
+      console.log(userData.value);
       function hasDuplicates(arr) {
         return arr.some(x => arr.indexOf(x) !== arr.lastIndexOf(x));
       }
-
-
 
       if (hasDuplicates(userData.value.priority)) {
         console.log("Duplicate elements found.");
         Swal.fire({
           title: "ADD Client!",
           text: "Gagal",
-          icon: "warning",
+          icon: "warning"
         });
+      } else {
+        console.log(userData.value);
+        let userDataAdd = userData.value;
+        userDataAdd.protocol = userData.value.priority[0];
+        const loginUrl = process.env.VUE_APP_BASE_URL + "api/users/client/";
+        // commit("auth_request");
+        axios
+          .post(loginUrl, userDataAdd, {
+            headers: {
+              token: localStorage.getItem("token")
+            }
+          })
+          .then(r => {
+            userData.value.priority = [];
+            userData.value.namePic = "";
+            userData.value.companyName = "";
+            userData.value.companyNumber = "";
+            userData.value.companyAddress = "";
+            userData.value.username = "";
+            userData.value.contactPic = "";
+            userData.value.whiteListIp = "";
+            userData.value.password = "";
+            userData.value.notifEmail = "";
+            userData.value.financeEmail = "";
 
-      }
-      else {
-        console.log(userData.value)
-        let userDataAdd = userData.value
-        userDataAdd.protocol = userData.value.priority[0]
-        const loginUrl =
-        process.env.VUE_APP_BASE_URL +
-        "api/users/client/";
-      // commit("auth_request");
-      axios
-        .post(loginUrl,userDataAdd, {
-          headers: {
-            token: localStorage.getItem("token"),
-          },
-        })
-        .then((r) => {
-          userData.value.priority =[];
-          userData.value.namePic = "";
-          userData.value.companyName = "";
-          userData.value.companyNumber = "";
-          userData.value.companyAddress = "";
-          userData.value.username = "";
-          userData.value.contactPic = "";
-          userData.value.whiteListIp = "";
-          userData.value.password = "";
-          userData.value.notifEmail = "";
-          userData.value.financeEmail = "";
-
-          if (r.data) {
+            if (r.data) {
+              Swal.fire({
+                title: "ADD Client!",
+                text: "Success",
+                icon: "success"
+              });
+            }
+            store.dispatch("fetchClients");
+            paramWindow.value = false;
+          })
+          .catch(error => {
+            console.log(error.response.data.message);
+            // commit("auth_error");
+            // localStorage.removeItem("token");
             Swal.fire({
               title: "ADD Client!",
-              text: "Success",
-              icon: "success",
+              text: "Gagal",
+              icon: "warning"
             });
-          }
-          store.dispatch("fetchClients");
-          paramWindow.value = false
-
-        })
-        .catch((error) => {
-          console.log(error.response.data.message)
-          // commit("auth_error");
-          // localStorage.removeItem("token");
-          Swal.fire({
-            title: "ADD Client!",
-            text: "Gagal",
-            icon: "warning",
+            // alert(error.message);
           });
-          // alert(error.message);
-        });
         console.log("No Duplicates found.");
-
       }
-    }
+    };
     const store = useStore();
     onMounted(async () => {
       await store.dispatch("fetchProtocol");
@@ -291,7 +326,6 @@ export default {
       userData,
       mdiTrashCan,
       mdiGithub
-
     };
   }
 };
