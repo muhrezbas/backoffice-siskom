33fae0ac249e0a2570db729bd41de325819f5d8c
Bagus Bimo <bagus.mulyanto@dompetkilat.co.id>
2021-12-12 20:00:42 +0700

    fix label in chart

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,14 +2,31 @@
   <title-bar :title-stack="titleStack" />
   <hero-bar>Dashboard</hero-bar>
   <main-section>
-    <select style="width:150px; margin-right: 25px" v-model="userData.filterTime" class="w-full">
+    <select
+      style="width:150px; margin-right: 25px"
+      v-model="userData.filterTime"
+      class="w-full"
+    >
       <option
-        v-for="option in [{ name: '10 Min', value: 10 }, { name: '15 Min', value: 15 }, { name: '30 Min', value: 30 }, { name: '1 Hour', value: 60 }, { name: '3 Hour', value: 240 }, { name: '6 Hour', value: 480 }, { name: '12 Hour', value: 960 }, { name: '1 Days', value: 1920 }, { name: '7 days', value: 10080 }, { name: '28 days', value: 40320 }, { name: 'All Time', value: 'alltime' }]"
+        v-for="option in [
+          { name: '6 Hour', value: 360 },
+          { name: '1 Days', value: 1440 },
+          { name: '7 days', value: 10080 },
+          { name: '28 days', value: 40320 },
+          { name: 'All Time', value: 'alltime' }
+        ]"
         :key="option"
         :value="option.value"
-      >{{ option.name }}</option>
+        >{{ option.name }}</option
+      >
     </select>
-    <jb-button class="mr-3" color="info" :icon="mdiSearchWeb" small @click="filter(perPage)" />
+    <jb-button
+      class="mr-3"
+      color="info"
+      :icon="mdiSearchWeb"
+      small
+      @click="filter(perPage)"
+    />
     <div class="grid grid-cols-1 gap-6 lg:grid-cols-2 mb-6">
       <card-widget
         class="tile"
@@ -22,28 +39,59 @@
         class="tile"
         color="text-yellow-500"
         :icon="mdiCheckboxMarked"
-        :number="sms.filter(el => el.statusSms !== undefined && el.prize !== null && el.statusSms.code == 0).length"
+        :number="
+          sms.filter(
+            el =>
+              el.statusSms !== undefined &&
+              el.prize !== null &&
+              el.statusSms.code == 0
+          ).length
+        "
         label="SMS Delivered"
       />
       <card-widget
         class="tile"
         color="text-purple-500"
         :icon="mdiFlash"
-        :number="sms.filter(el => el.statusSms !== undefined && el.prize !== null && el.statusSms.code == 0 && el.prize.akun == 'reguler').length"
+        :number="
+          sms.filter(
+            el =>
+              el.statusSms !== undefined &&
+              el.prize !== null &&
+              el.statusSms.code == 0 &&
+              el.prize.akun == 'reguler'
+          ).length
+        "
         label="SMS Blast"
       />
       <card-widget
         class="tile"
         color="text-green-500"
         :icon="mdiChartTimelineVariant"
-        :number="sms.filter(el => el.statusSms !== undefined && el.prize !== null && el.statusSms.code == 0 && el.prize.akun == 'premium').length"
+        :number="
+          sms.filter(
+            el =>
+              el.statusSms !== undefined &&
+              el.prize !== null &&
+              el.statusSms.code == 0 &&
+              el.prize.akun == 'premium'
+          ).length
+        "
         label="OTP"
       />
       <card-widget
         class="tile"
         color="text-red-500"
         :icon="mdiChartTimelineVariant"
-        :number="sms.filter(el => el.statusSms !== undefined && el.prize !== null && el.statusSms.code !== 0 || el.statusSms == undefined).length"
+        :number="
+          sms.filter(
+            el =>
+              (el.statusSms !== undefined &&
+                el.prize !== null &&
+                el.statusSms.code !== 0) ||
+              el.statusSms == undefined
+          ).length
+        "
         label="NOT SENT"
       />
     </div>
@@ -61,7 +109,9 @@
     </card-component>
     <!-- <title-bar :title-stack="titleStack" /> -->
 
-    <hero-bar v-if="$store.state.errorAccess == false" :control="true">Customer SMS Details List</hero-bar>
+    <hero-bar v-if="$store.state.errorAccess == false" :control="true"
+      >Customer SMS Details List</hero-bar
+    >
     <main-section v-if="$store.state.errorAccess == false">
       <card-component has-table>
         <sms-table v-if="this.sms" :sms="this.sms" checkable />
@@ -137,6 +187,7 @@ export default {
   //     return this.$store.state.sms;
   //   }
   // },
+
   setup() {
     const store = useStore();
     onMounted(async () => {
@@ -155,33 +206,38 @@ export default {
     const userData = computed(() =>
       reactive({
         toTime: new Date(-8640000000000000),
-        filterTime: 'alltime',
+        filterTime: "alltime"
         // smsBlast : sms.value.filter(el => el.prize !== null && el.statusSms !== undefined && el.prize.akun == "reguler" && el.statusSms.code == 0)
       })
-    )
+    );
     // const toTime = new Date(-8640000000000000);;
     // console.log(toTime.getTime(), "to Time")
 
     const titleStack = ref(["Admin", "Dashboard"]);
-    const sms = computed(() => store.state.sms.filter((admin) => {
-      return new Date(admin.createdAt).getTime() >= userData.value.toTime.getTime() &&
-        new Date(admin.createdAt).getTime() <= new Date()
-    }));
-    console.log(sms.value, "tester")
-    const filter = (payload) => {
-      if (userData.value.filterTime == 'alltime') {
-        userData.value.toTime = new Date(-8640000000000000)
-      }
-      else {
-        console.log(payload, "tesr")
+    const sms = computed(() =>
+      store.state.sms.filter(admin => {
+        return (
+          new Date(admin.createdAt).getTime() >=
+            userData.value.toTime.getTime() &&
+          new Date(admin.createdAt).getTime() <= new Date()
+        );
+      })
+    );
+    console.log(sms.value, "tester");
+
+    const filter = payload => {
+      if (userData.value.filterTime == "alltime") {
+        userData.value.toTime = new Date(-8640000000000000);
+      } else {
+        console.log(payload, "tesr");
         var d = new Date();
 
-
-        userData.value.toTime = new Date(d.setMinutes(d.getMinutes() - userData.value.filterTime))
-        console.log(userData.value.toTime)
+        userData.value.toTime = new Date(
+          d.setMinutes(d.getMinutes() - userData.value.filterTime)
+        );
+        console.log(userData.value.toTime);
       }
-
-    }
+    };
 
     const chartData = ref(null);
     const chartColors = {
@@ -190,7 +246,7 @@ export default {
         info: "purple",
         yellow: "yellow",
         blue: "blue",
-        red: "red",
+        red: "red"
         // danger: '#FF3860'
       }
     };
@@ -243,20 +299,7 @@ export default {
       };
     };
     const sampleChartData = (points = 12) => {
-      const labels = [
-        "January",
-        "February",
-        "March",
-        "April",
-        "May",
-        "Juni",
-        "Juli",
-        "Agustus",
-        "September",
-        "Oktober",
-        "Novermber",
-        "Desember"
-      ];
+      const labels = declareLabelRange();
 
       // for (let i = 1; i <= points; i++) {
       //   labels.push(`${i}`);
@@ -267,25 +310,42 @@ export default {
         datasets: [
           datasetObject(
             "primary",
-            sms.value.filter(el => el.prize !== null && el.statusSms !== null && el.prize.akun == "premium"),
+            sms.value.filter(
+              el =>
+                el.prize !== null &&
+                el.statusSms !== null &&
+                el.prize.akun == "premium"
+            ),
             labels
           ),
           datasetObject(
             "info",
             sms.value.filter(
-              el => el.prize !== null && el.statusSms !== undefined && el.prize.akun == "reguler" && el.statusSms.code == 0
+              el =>
+                el.prize !== null &&
+                el.statusSms !== undefined &&
+                el.prize.akun == "reguler" &&
+                el.statusSms.code == 0
             ),
             labels
           ),
           datasetObject("blue", sms.value, labels),
           datasetObject(
             "yellow",
-            sms.value.filter(el => el.statusSms !== undefined && el.statusSms.code == 0),
+            sms.value.filter(
+              el => el.statusSms !== undefined && el.statusSms.code == 0
+            ),
             labels
           ),
           datasetObject(
             "red",
-            sms.value.filter(el => el.statusSms !== undefined && el.prize !== undefined && el.statusSms.code !== 0 || el.statusSms == undefined),
+            sms.value.filter(
+              el =>
+                (el.statusSms !== undefined &&
+                  el.prize !== undefined &&
+                  el.statusSms.code !== 0) ||
+                el.statusSms == undefined
+            ),
             labels
           )
           // datasetObject('danger', points)
@@ -296,6 +356,106 @@ export default {
     const fillChartData = () => {
       chartData.value = sampleChartData();
     };
+
+    const todayHours = val => {
+      let days = [
+        "Minggu",
+        "Senin",
+        "Selasa",
+        "Rabu",
+        "Kamis",
+        "Jumat",
+        "Sabtu"
+      ];
+      let today = new Date(Date.now() - 1000 * (60 * val));
+      let currentDay = days[today.getDay()];
+      let currentHours = ("0" + today.getHours()).slice(-2);
+      let currentMinutes = ("0" + today.getMinutes()).slice(-2);
+      let formattedDate =
+        currentDay + ", " + currentHours + ":" + currentMinutes;
+      return formattedDate;
+    };
+
+    const todayDays = val => {
+      let days = [
+        "Minggu",
+        "Senin",
+        "Selasa",
+        "Rabu",
+        "Kamis",
+        "Jumat",
+        "Sabtu"
+      ];
+      let months = [
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec"
+      ];
+      let today = new Date(Date.now() - 1000 * (60 * val));
+      let currentDay = days[today.getDay()];
+      let currentMonth = months[today.getMonth()];
+      let formattedDate =
+        currentDay + ", " + today.getDate() + " " + currentMonth;
+      return formattedDate;
+    };
+
+    const chartLabelHours = interval => {
+      let data = [];
+      let counter = 0;
+      for (let i = 0; i < 12; i++) {
+        data[i] = todayHours(counter);
+        counter += interval;
+      }
+      return data.reverse();
+    };
+
+    const chartLabelDays = interval => {
+      let data = [];
+      let counter = 0;
+      for (let i = 0; i < 12; i++) {
+        data[i] = todayDays(counter);
+        counter += interval;
+      }
+      return data.reverse();
+    };
+
+    const declareLabelRange = () => {
+      switch (userData.value.filterTime) {
+        case 360:
+          return chartLabelHours(360);
+        case 1440:
+          return chartLabelDays(1440);
+        case 10080:
+          return chartLabelDays(10080);
+        case 40320:
+          return chartLabelDays(40320);
+        default:
+          return [
+            "January",
+            "February",
+            "March",
+            "April",
+            "May",
+            "Juni",
+            "Juli",
+            "Agustus",
+            "September",
+            "Oktober",
+            "Novermber",
+            "Desember"
+          ];
+      }
+    };
+
     const tkn = () => {
       var base64Url = localStorage.getItem("token").split(".")[1];
       //console.log(JSON.parse(window.atob(base64Url)));
