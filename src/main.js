542c152c870a40ce77f81574215c2455a4b1419a
Bagus Bimo <bagus.mulyanto@dompetkilat.co.id>
2022-01-03 15:01:34 +0700

    add range export

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import axios from "axios";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import Swal from 'sweetalert2'
+import Swal from "sweetalert2";
 
 import "./css/main.css";
 
@@ -19,50 +19,43 @@ import "./css/main.css";
 // store.dispatch("fetchUsers");
 // store.dispatch("fetchCountrys");
 
-axios.interceptors.response.use(undefined, function (error) {
+axios.interceptors.response.use(undefined, function(error) {
   if (error) {
-    let swall = true
-    console.log(error, "error ni")
+    let swall = true;
+    console.log(error, "error ni");
     const originalRequest = error.config;
     if (error.response.status === 401 && !originalRequest._retry) {
       originalRequest._retry = true;
       store.dispatch("logout");
-      console.log('intercept')
+      console.log("intercept");
       return router.push("/");
-    }
-    else {
-      let err
+    } else {
+      let err;
       if (error.response.status == 403) {
         if (error.response.config.method == "get") {
-          swall = false
+          swall = false;
           store.commit("basic", {
             key: "errorAccess",
             value: true
           });
-          console.log()
-
+          console.log();
         }
-        err = "Not Authorize"
+        err = "Not Authorize";
+      } else if (error.response.data == undefined) {
+        err = error.response;
+      } else if (error.response.data.message == undefined) {
+        err = JSON.stringify(error.response.data);
+      } else {
+        err = error.response.data.message;
       }
-      else if (error.response.data == undefined) {
-        err = error.response
-      }
-      else if (error.response.data.message == undefined) {
-        err = JSON.stringify(error.response.data)
-      }
-
-      else {
-        err = error.response.data.message
-      }
-      console.log(err, "cas")
+      console.log(err, "cas");
       // commit("auth_error");
       // localStorage.removeItem("token");
       if (swall == true) {
-
         Swal.fire({
           title: "ERROR!",
           text: err,
-          icon: "warning",
+          icon: "warning"
         });
       }
     }
